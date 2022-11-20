847e5c17a3ae633af7cd5e0710c96be67fd8fc25
Naitsabess <118657630+Naitsabess@users.noreply.github.com>
2022-11-20 11:00:32 +0100

    Fixed disable
    Using Javascript automatic garbage collection.

--- extension.js
+++ extension.js
@@ -166,7 +166,7 @@ function enable() {
 }
 
 function disable() {
-    protector.distroy();
+    protector = null;
 }
 
 //----------------------------------------------------------------------------------------
